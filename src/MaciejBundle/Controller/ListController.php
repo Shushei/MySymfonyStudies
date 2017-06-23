ee5620cfc51871740298534dc1dfe24e92c29f98
Shushei <DBShushei@gmail.com>
2017-06-23 14:53:37 +0200

    Delete Working

--- src/MaciejBundle/Controller/ListController.php
+++ src/MaciejBundle/Controller/ListController.php
@@ -13,9 +13,21 @@ class ListController extends Controller
 {
     public function showAction()
     {
+        
         $em =$this->getDoctrine()->getManager();
         $games= $em->getRepository('MaciejBundle:FormBase')->findAll();
-    
+        
+        $request = Request::createFromGlobals();
+        $request->getPathInfo();
+        $number = $request->get('wild');
+        
+        if (!empty($number))
+        {$delete = $em->getRepository('MaciejBundle:FormBase')->findOneById($number);
+        $em->remove($delete);
+        $em->flush();
+        $games= $em->getRepository('MaciejBundle:FormBase')->findAll();
+            return $this->render ('MaciejBundle:List:list.html.twig', ['games' => $games]);
+        }
       
        return $this->render ('MaciejBundle:List:list.html.twig', ['games' => $games]);
     }
