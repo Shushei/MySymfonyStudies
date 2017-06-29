ea3e958682636450d63cac4922bf49b2e4492bbf
Shushei <DBShushei@gmail.com>
2017-06-29 12:45:30 +0200

    Playing with associations

--- src/MaciejBundle/Entity/Companies.php
+++ src/MaciejBundle/Entity/Companies.php
@@ -5,7 +5,7 @@ namespace MaciejBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
@@ -23,11 +23,22 @@ class Companies
     private $id;
 
     /**
+     * @ORM\OneToMany(targetEntity="Games", mappedBy="company")
+     * 
+     */
+    private $games;
+
+    public function __construct()
+    {
+        $this->games = new ArrayCollection();
+        
+    }
+
+    /**
      * @ORM\Column(type="string", unique=true)
      * @Assert\NotBlank()
      */
     protected $company;
-   
 
     /**
      * @ORM\Column(type="date")
@@ -93,4 +104,38 @@ class Companies
         return $this->ownersurname = $ownersurname;
     }
 
+
+    /**
+     * Add game
+     *
+     * @param \MaciejBundle\Entity\Games $game
+     *
+     * @return Companies
+     */
+    public function addGame(\MaciejBundle\Entity\Games $game)
+    {
+        $this->games[] = $game;
+
+        return $this;
+    }
+
+    /**
+     * Remove game
+     *
+     * @param \MaciejBundle\Entity\Games $game
+     */
+    public function removeGame(\MaciejBundle\Entity\Games $game)
+    {
+        $this->games->removeElement($game);
+    }
+
+    /**
+     * Get games
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getGames()
+    {
+        return $this->games;
+    }
 }
